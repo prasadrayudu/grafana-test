5965c1c9d631fff5d8cef126132013bb01327edd
prasadrayudu <prasad.rayudu93@gmail.com>
2023-07-24 07:19:03 +0530

    added provider url and auth of dev

--- provider.tf
+++ provider.tf
@@ -1,3 +1,22 @@
+# Define the data block to access the workspace_api_key output from Terraform Cloud
+data "terraform_remote_state" "amg" {
+  backend = "remote"
+
+  config = {
+    organization = "sph"
+    workspaces = {
+      name = "mlops-eks-cluster-dev" # Replace with the name of the workspace where the amg module is running
+    }
+  }
+}
+
+# Use the data source to retrieve the workspace_api_key output from Terraform Cloud
+provider "grafana" {
+  url  = "https://${data.terraform_remote_state.amg.outputs.workspace_endpoint}"
+  auth = data.terraform_remote_state.amg.outputs.workspace_api_key
+}
+
+/*
 provider "grafana" {
   #url  = "https://${module.amg.workspace_endpoint}"
   #auth = module.amg.workspace_api_key
@@ -7,6 +26,7 @@ provider "grafana" {
   url  = var.grafana_url
   auth = var.grafana_auth_token
 
+  
 }
-
+*/
 
